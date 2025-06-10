548f845279c260ce29d43fcf62d5496ff7d30afa
Lucas Della Sala <lsala@webimpacto.es>
2025-06-10 17:01:00 -0300

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN ?? '*',
+  });
   await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
 }
 bootstrap().catch((error) => {
